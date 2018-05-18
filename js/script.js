17a641f16f5c092a43bf7084f630741e38bbd135
Janicek2 <bjanicki82@gmail.com>
2018-05-18 20:15:46 +0200

    Add isOnStock

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-function laptop(screen, disk, name, processor) {
+function Laptop(screen, disk, name, processor) {
+    var self = this;
     this.screen = screen;
     this.disk = disk;
     this.name = name;
     this.processor = processor;
 }
 
-laptop.prototype.screenOnConsole = function() {
+Laptop.prototype.screenOnConsole = function() {
     console.log('Nazwa komputera to' + " " + this.name + "Ma on ekran o przekątnek" + " " + this.screen + " " + "maon dysk o pojemności" + " " + this.disk + "oraz procesor" + this.processor);
 };
 
-var dell = new laptop ("13", "SSD", "Dell", "I5");
-var Mcbook = new laptop ('14', 'HD', 'Mcbook', 'I6');
-var Zenit = new laptop ('16', 'SSD', 'Zenit', 'I3');
+Laptop.prototype.inOnStock = function() {
+    var stock = Math.round(Math.random());
+    console.log(stock);
+    if (stock < 1) {
+        console.log('Nie ma' + " " + this.name + " " + 'na stanie');
+    } else {
+        console.log ('Jest' + " " + this.name + " " + 'na stanie');
+    }
+};
+
+var dell = new Laptop ("13", "SSD", "Dell", "I5");
+var mcbook = new Laptop ('14', 'HD', 'Mcbook', 'I6');
+var zenit = new Laptop ('16', 'SSD', 'Zenit', 'I3');
 
 dell.screenOnConsole();
-Mcbook.screenOnConsole();
-Zenit.screenOnConsole();
+mcbook.screenOnConsole();
+zenit.screenOnConsole();
+dell.inOnStock();
+mcbook.inOnStock();
+zenit.inOnStock();
